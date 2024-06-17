02eab151b5efd3bd6616dacaf2cc1d7b8668f74e
Doneux <85197474+Doneux@users.noreply.github.com>
2024-06-17 14:02:31 -0700

    Update kinematicsUI.py

--- kinematicsUI.py
+++ kinematicsUI.py
@@ -6,8 +6,54 @@ from tkinter import *
 import math
 import vectors #vectors.py is my own vector script. please make sure you have it installed to the same folder as this file.
 
-
 #main program
+#displacement variables
+Xi = 0
+Xf = 0
+dX = Xf - Xi
+#time
+Ti = 0
+Tf = 0
+dT = Tf - Ti
+#Velocity
+Vi = 0
+Vf = 0
+dV = Vi - Vf
+V = dX / dT
+#Acceleration
+Ai = 0
+Af = 0
+dA = Af - Ai
+A = dV / dT
+#Mass
+M = 0
+#Force
+F = M * A
+#Possible ways to solve for each:
+def solve(Xi, Xf, dX, Ti, Tf, dT, Vi, Vf, dV, V, Ai, Af, dA, A, M, F):
+    #solve_acceleration:
+    A = dV/dT
+    A = F/M
+    Af = dA + Ai
+    Ai = dA - Af
+    #solve_mass:
+    M = F / A
+    #solve_velocity:
+    dV = A * dT
+    Vf = dV + Vi
+    Vi = dV - Vf
+    #solve_force:
+    F = M * A
+    #solve_displacement:
+    dX = V * dT
+    Xf = dX + Xi
+    Xi = dX - Xf
+    #solve_time:
+    dT = dX / V
+    Tf = dT + Ti
+    Ti = dT - Tf
+    #continuously run solution functions until no variables have changed
+
 
 #window setup
 win = Tk()
