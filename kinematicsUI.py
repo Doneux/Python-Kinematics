fb662335634cf075247726e1ba5937e9da8b4fd6
Doneux <85197474+Doneux@users.noreply.github.com>
2024-06-29 21:09:45 -0700

    Update kinematicsUI.py

--- kinematicsUI.py
+++ kinematicsUI.py
@@ -6,11 +6,6 @@ from tkinter import *
 import math
 import vectors #vectors.py is my own vector script. please make sure you have it installed to the same folder as this file.
 
-#this is currently buggy, as placeholder zeros will cause overwriting in solution process
-#will likely add separate solution sections/buttons for each equation, so that the givens themselves are not modified
-#but in the meantime i may look for a solution by analyzing what can be solved based on which givens have input and which do not
-
-
 #main program
 #displacement variables
 Xi = 0.0
@@ -100,7 +95,7 @@ def solve(Xi, Xf, dX, Ti, Tf, dT, Vi, Vf, dV, V, Ai, Af, dA, A, M, F):
 #window setup
 win = Tk()
 win.title("Python Kinematics")
-win.geometry('250x500')
+win.geometry('250x1000')
 #Window setup - 9 columns, middle column at 5
 title = Label(text="Python Kinematics Solver - Seth Doneux")
 title.grid(columnspan=9)
@@ -208,46 +203,42 @@ ftitle.grid()
 fbox = Entry(width=5)
 fbox.grid(row=19, column=2, columnspan=3)
 
-#Sets values to screen
-def display_values(Xi, Xf, dX, Ti, Tf, dT, Vi, Vf, dV, V, Ai, Af, dA, A, M, F):
-    xibox.delete(0, END)
-    xibox.insert(0, str(Xi))
-    xfbox.delete(0, END)
-    xfbox.insert(0, str(Xf))
-    dxbox.delete(0, END)
-    dxbox.insert(0, str(dX))
-    tibox.delete(0, END)
-    tibox.insert(0, str(Ti))
-    tfbox.delete(0, END)
-    tfbox.insert(0, str(Tf))
-    dtbox.delete(0, END)
-    dtbox.insert(0, str(dT))
-    vibox.delete(0, END)
-    vibox.insert(0, str(Vi))
-    vfbox.delete(0, END)
-    vfbox.insert(0, str(Vf))
-    dvbox.delete(0, END)
-    dvbox.insert(0, str(dV))
-    vabox.delete(0, END)
-    vabox.insert(0, str(V))
-    aibox.delete(0, END)
-    aibox.insert(0, str(Ai))
-    afbox.delete(0, END)
-    afbox.insert(0, str(Af))
-    dabox.delete(0, END)
-    dabox.insert(0, str(dA))
-    aabox.delete(0, END)
-    aabox.insert(0, str(A))
-    mbox.delete(0, END)
-    mbox.insert(0, str(M))
-    fbox.delete(0, END)
-    fbox.insert(0, str(F))
+# displays. the delete commands are leftovers from debugging, they're not hurting anyone (i'll remove them soon)
+xibox.delete(0, END)
+xibox.insert(0, str(Xi))
+xfbox.delete(0, END)
+xfbox.insert(0, str(Xf))
+dxbox.delete(0, END)
+dxbox.insert(0, str(dX))
+tibox.delete(0, END)
+tibox.insert(0, str(Ti))
+tfbox.delete(0, END)
+tfbox.insert(0, str(Tf))
+dtbox.delete(0, END)
+dtbox.insert(0, str(dT))
+vibox.delete(0, END)
+vibox.insert(0, str(Vi))
+vfbox.delete(0, END)
+vfbox.insert(0, str(Vf))
+dvbox.delete(0, END)
+dvbox.insert(0, str(dV))
+vabox.delete(0, END)
+vabox.insert(0, str(V))
+aibox.delete(0, END)
+aibox.insert(0, str(Ai))
+afbox.delete(0, END)
+afbox.insert(0, str(Af))
+dabox.delete(0, END)
+dabox.insert(0, str(dA))
+aabox.delete(0, END)
+aabox.insert(0, str(A))
+mbox.delete(0, END)
+mbox.insert(0, str(M))
+fbox.delete(0, END)
+fbox.insert(0, str(F))
 
-#Call it once in setup for good measure
-display_values(Xi, Xf, dX, Ti, Tf, dT, Vi, Vf, dV, V, Ai, Af, dA, A, M, F)
-
-#Solver function
-def solve_unknowns():
+#get knowns
+def get_knowns():
     Xi = float(xibox.get())
     Xf = float(xfbox.get())
     dX = float(dxbox.get())
@@ -264,12 +255,101 @@ def solve_unknowns():
     A = float(aabox.get())
     M = float(mbox.get())
     F = float(fbox.get())
-    Xi, Xf, dX, Ti, Tf, dT, Vi, Vf, dV, V, Ai, Af, dA, A, M, F = solve(Xi, Xf, dX, Ti, Tf, dT, Vi, Vf, dV, V, Ai, Af, dA, A, M, F)
-    display_values(Xi, Xf, dX, Ti, Tf, dT, Vi, Vf, dV, V, Ai, Af, dA, A, M, F)
+    return Xi, Xf, dX, Ti, Tf, dT, Vi, Vf, dV, V, Ai, Af, dA, A, M, F
 
-solvetitle = Label(text="Solve Unknown Variables")
+#solve equations
+def solve_equations():
+    Xi, Xf, dX, Ti, Tf, dT, Vi, Vf, dV, V, Ai, Af, dA, A, M, F = get_knowns()
+    # solve_force:
+    F = M * A
+    forcefinal = Label(text=f"F = MA = {F}")
+    forcefinal.grid(columnspan= 9)
+    
+    # solve_acceleration:
+    dA = Af - Ai
+    try:
+        A = dV / dT
+    except:
+        pass
+    try:
+        A = F / M
+    except:
+        pass
+    Af = dA + Ai
+    Ai = dA - Af
+    
+    accel1 = Label(text=f"A = dV/dT = {A}")
+    accel1.grid(columnspan= 9)
+    accel2 = Label(text=f"A = F/M = {A}")
+    accel2.grid(columnspan= 9)
+    accel3 = Label(text=f"Af = dA + Ai = {Af}")
+    accel3.grid(columnspan= 9)
+    accel4 = Label(text=f"Ai = dA - Af = {Ai}")
+    accel4.grid(columnspan= 9)
+    
+    # solve_mass:
+    try:
+        M = F / A
+    except:
+        pass
+    massfinal = Label(text=f"M = F/A = {M}")
+    massfinal.grid(columnspan= 9)
+    
+    # solve_velocity:
+    dV = Vf - Vi
+    dV = A * dT
+    Vf = dV + Vi
+    Vi = dV - Vf
+    try:
+        V = dX / dT
+    except:
+        pass
+    velocity1 = Label(text=f"dV = Vf - Vi = {dV}")
+    velocity1.grid(columnspan= 9)
+    velocity2 = Label(text=f"dV = A * dT = {dV}")
+    velocity2.grid(columnspan= 9)
+    velocity3 = Label(text=f"Vf = dV + Vi = {Vf}")
+    velocity3.grid(columnspan= 9)
+    velocity4 = Label(text=f"Vi = dV - Vf = {Vi}")
+    velocity4.grid(columnspan= 9)
+    velocity5 = Label(text=f"V = dX / dT = {V}")
+    velocity5.grid(columnspan= 9)
+    
+    # solve_displacement:
+    dX = Xf - Xi
+    dX = V * dT
+    Xf = dX + Xi
+    Xi = dX - Xf
+    displacement1 = Label(text=f"dX = Xf - Xi = {dX}")
+    displacement1.grid(columnspan= 9)
+    displacement2 = Label(text=f"dX = V * dT = {dX}")
+    displacement2.grid(columnspan= 9)
+    displacement3 = Label(text=f"Xf = dX + Xi = {Xf}")
+    displacement3.grid(columnspan= 9)
+    displacement4 = Label(text=f"Xi = dX - Xf = {Xi}")
+    displacement4.grid(columnspan= 9)
+    
+    # solve_time:
+    dT = Tf - Ti
+    try:
+        dT = dX / V
+    except:
+        pass
+    Tf = dT + Ti
+    Ti = dT - Tf
+    time1 = Label(text=f"dT = Tf - Ti = {dT}")
+    time1.grid(columnspan= 9)
+    time2 = Label(text=f"dT = dX / V = {dT}")
+    time2.grid(columnspan= 9)
+    time3 = Label(text=f"Tf = dT + Ti = {Tf}")
+    time3.grid(columnspan= 9)
+    time4 = Label(text=f"Ti = dT - Tf = {Ti}")
+    time4.grid(columnspan= 9)
+
+
+solvetitle = Label(text="Solve Kinematic Equations")
 solvetitle.grid(columnspan= 9)
-solvebutton = Button(text="Solve", command=solve_unknowns)
+solvebutton = Button(text="Solve", command=solve_equations)
 solvebutton.grid(columnspan=9)
 
 
